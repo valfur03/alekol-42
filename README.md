78da575da3acbf45780c67f58e74aa82e74ca5e6
valfur03 <valentin.furmanek@gmail.com>
2022-12-02 12:16:50 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alekol 42
+
+Discord bot to automatically add or remove a role whether you are logged at 42 school.
---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Web() {
+  return (
+    <div>
+      <h1>Alekol 42</h1>
+    </div>
+  );
+}
